99639414a7b48439f193918804ea379e4bc2cd28
UsmanAli90 <usmanmanagement23@gmail.com>
2024-05-07 23:49:41 +0500

    DataBase Connected

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const app = express()
 
+mongoose.connect('mongodb://localhost:27017/Test').then((result) =>
+    app.listen(3000)).catch((err) =>
+        console.log(err))
+
+
 app.set('view engine', 'ejs')
 
-
-app.listen(3000);
 app.use(express.static('public'))
 
 
 app.get('/', (req, res) => {
     res.render('index', { title: 'Home' })
 })
-
 
 app.get('/login', (req, res) => {
     res.render('login', { title: 'Login' })
